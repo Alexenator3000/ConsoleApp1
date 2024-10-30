30454d29c5f883af26ac6f68474b3986743eb591
Alexenator3000 <144926889+Alexenator3000@users.noreply.github.com>
2024-10-30 19:01:33 +0300

    jgfk

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyApp2
+{   
+    class Stack<T>
+    {
+        Stack<T> items = new Stack<T>();
+        private T _item;
+
+        public Stack()
+        {
+        }
+
+        public void Pop()
+        { 
+            items.Pop();
+        }
+
+        public void Push(T item)
+        { 
+            items.Push(item);
+        }
+    }
+    
+    class Box<T>
+    {
+        private T _item;
+        public List<T> items = new List<T>();
+        public Box()
+        {}
+        public void GetItem()
+        {
+            Console.WriteLine(_item);
+        }
+
+        public void SetItem(T item) 
+        {
+            _item = item;
+            items.Add(item);
+        }
+    }
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Box<int> item = new Box<int>();
+            Stack<int> stack = new Stack<int>();
+            item.SetItem(34);
+            item.GetItem();
+            stack.Push(52);
+            stack.Pop();
+        }
+    }
+}
